1f13437e80ae9c26639d8945688816304c418d5e
Reuben <reuben.hillyer@hotmail.co.uk>
2025-03-25 09:51:52 +0000

    Add unit test to assert that the closed auction event was raised after a message

---
+++ AuctionSniper.UnitTests/MessageTranslatorTests.cs
@@ -0,0 +1,28 @@
+﻿using AuctionSniper.XMPP;
+using Shouldly;
+using XmppDotNet.Xmpp.Client;
+
+namespace AuctionSniper.UnitTests
+{
+    public class MessageTranslatorTests
+    {
+        private bool closeEventRaised = false;
+
+        [Fact]
+        public void NotifiesAuctionClosedWhenCloseMessageReceived()
+        {
+            var message = new Message(string.Empty, SouthabeeStandards.CLOSE_REQUEST);
+            var translator = new MessageListener();
+            translator.CloseMessageReceived += CloseMessageReceived;
+
+            translator.ProcessMessage(null, message);
+
+            closeEventRaised.ShouldBe(true);
+        }
+
+        private void CloseMessageReceived(object? sender, EventArgs e)
+        {
+            closeEventRaised = true;
+        }
+    }
+}
