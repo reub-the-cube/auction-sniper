8eb9ca121a029c3b952c64ff480dfdb4af96c06a
Reuben <reuben.hillyer@hotmail.co.uk>
2025-03-25 16:14:18 +0000

    Remove some duplication from AuctionSniperTests

--- AuctionSniper.UnitTests/AuctionSniperTests.cs
+++ AuctionSniper.UnitTests/AuctionSniperTests.cs
@@ -5,12 +5,18 @@ namespace AuctionSniper.UnitTests
 {
     public class AuctionSniperTests
     {
+        private readonly Mock<Auction> auction = new();
+        private readonly Mock<SniperListener> sniperListener = new();
+        private readonly Core.AuctionSniper auctionSniper;
+
+        public AuctionSniperTests()
+        {
+            auctionSniper = new Core.AuctionSniper(auction.Object, sniperListener.Object);
+        }
+
         [Fact]
         public void ReportsLostWhenAuctionCloses()
         {
-            var sniperListener = new Mock<Core.SniperListener>();
-            var auctionSniper = new Core.AuctionSniper(null, sniperListener.Object);
-
             auctionSniper.AuctionClosed();
 
             sniperListener.Verify(v => v.SniperLost(), Times.Once());
@@ -19,10 +25,6 @@ namespace AuctionSniper.UnitTests
         [Fact]
         public void BidsHigherAndReportsBiddingWhenNewPriceArrives()
         {
-            var sniperListener = new Mock<SniperListener>();
-            var auction = new Mock<Auction>();
-            var auctionSniper = new Core.AuctionSniper(auction.Object, sniperListener.Object);
-
             auctionSniper.CurrentPrice(1001, 25);
 
             sniperListener.Verify(v => v.SniperBidding(), Times.AtLeastOnce());
