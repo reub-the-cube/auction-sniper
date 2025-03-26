a28a476e66c21a87d58f5963ec8b61605126045c
Reuben <reuben.hillyer@hotmail.co.uk>
2025-03-26 10:46:17 +0000

    [GREEN / REFACTOR] - update unit tests to provide the SniperId to the translator

--- AuctionSniper.UnitTests/SniperTranslatorTests.cs
+++ AuctionSniper.UnitTests/SniperTranslatorTests.cs
@@ -7,12 +7,14 @@ namespace AuctionSniper.UnitTests
 {
     public class SniperTranslatorTests
     {
+        private const string SNIPER_ID = "test_sniper";
+
         [Fact]
         public void NotifiesAuctionClosedWhenCloseMessageReceived()
         {
             var message = new Message(string.Empty, SouthabeeStandards.CLOSE_REQUEST);
             var auctionEventListener = new Mock<IAuctionEventListener>();
-            var translator = new SniperTranslator(auctionEventListener.Object);
+            var translator = new SniperTranslator(auctionEventListener.Object, SNIPER_ID);
 
             translator.ProcessMessage(message);
 
@@ -24,7 +26,7 @@ namespace AuctionSniper.UnitTests
         {
             var message = new Message(string.Empty, string.Format(SouthabeeStandards.REPORT_PRICE_EVENT, 192, 7, "Someone else"));
             var auctionEventListener = new Mock<IAuctionEventListener>();
-            var translator = new SniperTranslator(auctionEventListener.Object);
+            var translator = new SniperTranslator(auctionEventListener.Object, SNIPER_ID);
 
             translator.ProcessMessage(message);
 
