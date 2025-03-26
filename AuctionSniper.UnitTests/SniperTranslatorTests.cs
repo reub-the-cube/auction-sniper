660cc308ac4673835a92084ce016d62ed8b65171
Reuben <reuben.hillyer@hotmail.co.uk>
2025-03-26 10:50:21 +0000

    [RED, not compiling, no FromSniper defn] - Add a new failing test to verify that a bid came from the sniper

--- AuctionSniper.UnitTests/SniperTranslatorTests.cs
+++ AuctionSniper.UnitTests/SniperTranslatorTests.cs
@@ -32,5 +32,17 @@ namespace AuctionSniper.UnitTests
 
             auctionEventListener.Verify(v => v.CurrentPrice(192, 7, PriceSource.FromOtherBidder), Times.Once());
         }
+
+        [Fact]
+        public void NotifiesBidDetailsWhenCurrentPriceMessageReceivedFromSniper()
+        {
+            var message = new Message(string.Empty, string.Format(SouthabeeStandards.REPORT_PRICE_EVENT, 234, 5, SNIPER_ID));
+            var auctionEventListener = new Mock<IAuctionEventListener>();
+            var translator = new SniperTranslator(auctionEventListener.Object, SNIPER_ID);
+
+            translator.ProcessMessage(message);
+
+            auctionEventListener.Verify(v => v.CurrentPrice(234, 5, PriceSource.FromSniper), Times.Once());
+        }
     }
 }
